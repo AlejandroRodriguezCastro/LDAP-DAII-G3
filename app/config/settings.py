68babf3e28d12fc1020c7547859c23e98eedefe8
AlejandroRodriguezCastro <alejandrogrc@gmail.com>
2025-09-25 02:16:16 -0300

    [ADD] Register additional user exception handlers: added handlers for UserLockedDownError and UserInvalidCredentialsError in exception_config.py. [ADD] Enhance settings configuration: imported parse_expiration and added token_expiration_timedelta property in settings.py.

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,6 +2,8 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
+from app.utils.helpers.expiration_parser import parse_expiration
+
 class Settings(BaseSettings):
     APP_NAME: str
     LOG_LEVEL: str
@@ -11,7 +13,12 @@ class Settings(BaseSettings):
     LDAP_BIND_DN: str
     LDAP_BIND_PASSWORD: str
     SECRET_KEY: str
+    TOKEN_EXPIRATION: str
     model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), ".env"), extra="ignore")
+
+    @property
+    def token_expiration_timedelta(self):
+        return parse_expiration(self.TOKEN_EXPIRATION)
         
 @lru_cache()
 def get_settings():
