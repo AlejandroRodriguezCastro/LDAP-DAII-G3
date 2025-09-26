86030930f3f8db2a053cf6ed73ef6135e5272043
AlejandroRodriguezCastro <alejandrogrc@gmail.com>
2025-09-25 23:45:22 -0300

    [ADD] Implement role exception handlers: added custom exceptions and handlers for role-related errors, including not found, already exists, invalid data, creation and deletion failures, and unauthorized actions.

--- app/config/exception_config.py
+++ app/config/exception_config.py
@@ -19,6 +19,21 @@ from app.handlers.errors.user_exception_handlers import (
     UserInvalidCredentialsError
 )
 
+from app.handlers.errors.role_exception_handlers import (
+    role_not_found_exception_handler,
+    role_already_exists_exception_handler,
+    invalid_role_data_exception_handler,
+    failure_role_creation_exception_handler,
+    failure_role_deletion_exception_handler,
+    unauthorized_role_exception_handler,
+    RoleNotFoundError,
+    RoleAlreadyExistsError,
+    InvalidRoleDataError,
+    FailureRoleCreationError,
+    FailureRoleDeletionError,
+    UnauthorizedRoleError
+)
+
 logger = structlog.get_logger()
 
 def register_exception_handlers(app: FastAPI):
@@ -31,3 +46,12 @@ def register_exception_handlers(app: FastAPI):
     app.add_exception_handler(FailureUserDeletionError, failure_user_deletion_handler)
     app.add_exception_handler(UserLockedDownError, user_locked_down_handler)
     app.add_exception_handler(UserInvalidCredentialsError, user_invalid_credentials_handler)
+    
+    logger.info("Registering role exception handlers with FastAPI app.")
+    app.add_exception_handler(RoleNotFoundError, role_not_found_exception_handler)
+    app.add_exception_handler(RoleAlreadyExistsError, role_already_exists_exception_handler)
+    app.add_exception_handler(InvalidRoleDataError, invalid_role_data_exception_handler)
+    app.add_exception_handler(FailureRoleCreationError, failure_role_creation_exception_handler)
+    app.add_exception_handler(FailureRoleDeletionError, failure_role_deletion_exception_handler)
+    app.add_exception_handler(UnauthorizedRoleError, unauthorized_role_exception_handler)
+    
